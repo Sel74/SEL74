061afb822e4def0a6fca37c7440da3e1ef086d38
labodunet <nbonin@labodunet.fr>
2023-04-18 10:54:41 +0200

    remove some page and change description meta tag

--- theme/src/config/settings.js
+++ theme/src/config/settings.js
@@ -1,7 +1,7 @@
 export default {
 	title: `Sytème d'échange local | Annecy`,
-	description: `Le site du SEL d'annecy`,
-	url: `https://odyssey-theme.littlesticks.dev`, // No trailing slash!
+	description: `Bienvenu sur le site du SEL d'annecy. SEL signifie Système d\'Echange Local. Pour un monde plus humain, plus solidaire, rjoignez-nous!`,
+	url: `https://www.sel74.fr`, // No trailing slash!
 	name: `SEL74`, // The short name of the business or brand name. Used for things like the copyright in the footer.
   enableThemeSwitcher: false,
   showLittleSticksPlug: false, // Disable this if you want to remove the Little Sticks plug from the footer. (╯°□°)╯︵ ┻━┻
