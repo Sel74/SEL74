d8e646a9a7e67b18912d9396532b7782fc7aa356
labodunet <nbonin@labodunet.fr>
2023-04-18 11:11:00 +0200

    orthographe!

--- theme/src/config/settings.js
+++ theme/src/config/settings.js
@@ -1,6 +1,6 @@
 export default {
 	title: `Sytème d'échange local | Annecy`,
-	description: `Bienvenu sur le site du SEL d'annecy. SEL signifie Système d\'Echange Local. Pour un monde plus humain, plus solidaire, rjoignez-nous!`,
+	description: `Bienvenu sur le site du SEL d'annecy. SEL signifie Système d\'Echange Local. Pour un monde plus humain, plus solidaire, rejoignez-nous!`,
 	url: `https://www.sel74.fr`, // No trailing slash!
 	name: `SEL74`, // The short name of the business or brand name. Used for things like the copyright in the footer.
   enableThemeSwitcher: false,
